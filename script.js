68736eba2afdf5420a2bd2d33cc28053466554a0
Vishalgithub0987 <vishalsangeeth71@gmail.com>
2025-03-22 11:36:47 +0530

    done with projects

--- script.js
+++ script.js
@@ -7,13 +7,35 @@ $(document).ready(function() {
         $('.navbar').removeClass("sticky");
     }
   });
+
   // toggle menu/navbar scripts
+
   $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
-  })
-          
+  });
 
+  // owl carousel scripts 
+  $('.carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause:true,
+    responsive: {
+      0:{
+        items: 1,
+        nav: false
+      },
+      600:{
+        items: 2,
+        nav: false
+      },
+      1000:{
+        items: 3,
+        nav: false
+      }
+    }
+  });      
 });
 
 
