179357ca84b1f2aff85573069a5369268723d1d0
Vishalgithub0987 <vishalsangeeth71@gmail.com>
2025-03-22 19:02:28 +0530

    done with project

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ $(document).ready(function() {
     if(this.scrollY > 20) {
       $('.navbar').addClass("sticky");
     }else{
-        $('.navbar').removeClass("sticky");
+      $('.navbar').removeClass("sticky");
     }
+    if(this.scrollY > 500){
+      $('.scroll-up-btn').addClass("show");
+    }else{
+      $('.scroll-up-btn').removeClass("show");
+    }
+
+  });
+
+  // slide-up scripts 
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0});
+
   });
 
   // toggle menu/navbar scripts
@@ -14,6 +26,30 @@ $(document).ready(function() {
     $('.navbar .menu').toggleClass("active");
     $('.menu-btn i').toggleClass("active");
   });
+
+
+  // typing animation scripts 
+ var typed = new Typed(".typing",{
+  strings:["Software Developer","Designer","Freelancer","Web Developer"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop: true
+
+ });
+
+ var typed = new Typed(".typing-2",{
+  strings:["Software Developer","Designer","Freelancer","Web Developer"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop: true
+
+ });
+
+
+
+
+
+
 
   // owl carousel scripts 
   $('.carousel').owlCarousel({
